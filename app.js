c69b9600bbdf37fe117a379bcfa1dd0b1b0991e1
ViniciusFreiry <viniciusfreiry@gmail.com>
2025-05-13 16:25:30 -0300

    Adding Scope

---
+++ app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do Número Secreto';
+
+let pargrafo = document.querySelector('p');
+pargrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function verificarChute() {
+    console.log("bah");
+}
